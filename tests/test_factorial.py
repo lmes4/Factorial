20231763ab6c7f966faf56c1511536e84f6d7514
Lorena E <lorenasimarro4@gmail.com>
2026-06-08 16:41:05 +0200

    test factorial

---
+++ tests/test_factorial.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import Page, expect
+    
+def test_calcular_factorial_numero_entero_positivo (page: Page):
+    print("Given la persona usuaria visita la pagina de Factorial")
+    page.goto("https://qainterview.pythonanywhere.com/")
+    print("When introduce un numero entero positivo")
+    page.get_by_role("textbox", name="Enter an integer").fill("3")
+    print("And hace click en calcular")
+    page.get_by_role("button", name="Calculate!").click()
+    print("Then debe ver el resultado del factorial del numero esperado")
+    expect(page.locator("#resultDiv")).to_contain_text("6")
+
+def test_calcular_factorial_valor_nonumerico (page: Page): 
+    print("Given la persona usuaria visita la pagina de Factorial")  
+    page.goto("https://qainterview.pythonanywhere.com/")
+    print("When introduce un valor de texto")
+    page.get_by_role("textbox", name="Enter an integer").fill("texto")
+    print("And hace click en calcular")
+    page.get_by_role("button", name="Calculate!").click()
+    print("Then debe ver un mensaje de error")
+    expect(page.get_by_text("Please enter an integer")).to_be_visible()
+
+#Se desactiva test porque genera error https://adalabequipo4.atlassian.net/browse/PF-2
+def test_calcular_factorial_valor_negativo (page: Page):
+    print("Given la persona usuaria visita la pagina de Factorial")
+    page.goto("https://qainterview.pythonanywhere.com/")
+    print("When introduce un valor numero negativo")
+    page.get_by_role("textbox", name="Enter an integer").fill("-1")
+    print("And hace click en calcular")
+    page.get_by_role("button", name="Calculate!").click()
+    print("Then debe ver un mensaje de error")
+    expect(page.get_by_text("Please enter an integer")).to_be_visible()
+
--- tests/test_visit_page.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import Page, expect
-
-def test_visit(page: Page):
-    print("Given user visit homepage")
-    page.goto("https://bootcampqa.com")
-    
-
